6ab323069a4744945bb754b328af2bd4aadd0bff
nanyang <836535111.@qq.com>
2021-03-03 16:52:03 +0800

    上传雷达图、（能力图）

---
+++ Assets/雷达图/RadarChart/Scripts/Stats.cs
@@ -0,0 +1,107 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading the Code Monkey Utilities
+    I hope you find them useful in your projects
+    If you have any questions use the contact form
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats {
+
+    public event EventHandler OnStatsChanged;
+
+    public static int STAT_MIN = 0;
+    public static int STAT_MAX = 20;
+
+    public enum Type {
+        Attack,
+        Defence,
+        Speed,
+        Mana,
+        Health,
+    }
+
+    private SingleStat attackStat;
+    private SingleStat defenceStat;
+    private SingleStat speedStat;
+    private SingleStat manaStat;
+    private SingleStat healthStat;
+
+    public Stats(int attackStatAmount, int defenceStatAmount, int speedStatAmount, int manaStatAmount, int healthStatAmount, int[] maxValues)
+    {
+        attackStat = new SingleStat(attackStatAmount, maxValues[0]);
+        defenceStat = new SingleStat(defenceStatAmount, maxValues[1]);
+        speedStat = new SingleStat(speedStatAmount, maxValues[2]);
+        manaStat = new SingleStat(manaStatAmount, maxValues[3]);
+        healthStat = new SingleStat(healthStatAmount, maxValues[4]);
+    }
+
+
+    private SingleStat GetSingleStat(Type statType) {
+        switch (statType) {
+        default:
+        case Type.Attack:       return attackStat;
+        case Type.Defence:      return defenceStat;
+        case Type.Speed:        return speedStat;
+        case Type.Mana:         return manaStat;
+        case Type.Health:       return healthStat;
+        }
+    }
+    
+    public void SetStatAmount(Type statType, int statAmount,int max) {
+        GetSingleStat(statType).SetStatAmount(statAmount, max);
+        if (OnStatsChanged != null) OnStatsChanged(this, EventArgs.Empty);
+    }
+
+    public void IncreaseStatAmount(Type statType,int max) {
+        SetStatAmount(statType, GetStatAmount(statType) + 1, max);
+    }
+
+    public void DecreaseStatAmount(Type statType,int max) {
+        SetStatAmount(statType, GetStatAmount(statType) - 1,max);
+    }
+
+    public int GetStatAmount(Type statType) {
+        return GetSingleStat(statType).GetStatAmount();
+    }
+
+    public float GetStatAmountNormalized(Type statType,int max) {
+        return GetSingleStat(statType).GetStatAmountNormalized(max);
+    }
+
+
+
+    /*
+     * Represents a Single Stat of any Type
+     * */
+    private class SingleStat {
+
+        private int stat;
+
+        public SingleStat(int statAmount, int maxValue) {
+            SetStatAmount(statAmount, maxValue);
+        }
+
+        public void SetStatAmount(int statAmount,int maxValue) {
+            stat = Mathf.Clamp(statAmount, STAT_MIN, maxValue);
+        }
+
+        public int GetStatAmount() {
+            return stat;
+        }
+
+        public float GetStatAmountNormalized(int max) {
+            //return (float)stat / STAT_MAX;
+             return (float)stat / max;
+        }
+    }
+}
---
+++ Assets/雷达图/RadarChart/Scripts/UI_StatsRadarChart.cs
@@ -0,0 +1,133 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading the Code Monkey Utilities
+    I hope you find them useful in your projects
+    If you have any questions use the contact form
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_StatsRadarChart : MonoBehaviour
+{
+
+    [SerializeField] private Material radarMaterial;
+    [SerializeField] private Texture2D radarTexture2D;
+
+    private Stats stats;
+    private CanvasRenderer radarMeshCanvasRenderer;
+
+    public float radarChartSize = 145f;
+
+    public int[] m_maxValues;
+
+
+    private void Awake()
+    {
+        radarMeshCanvasRenderer = transform.Find("radarMesh").GetComponent<CanvasRenderer>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DrawLine());
+    }
+
+    IEnumerator DrawLine()
+    {
+        while (true)
+        {
+            List<int> value = new List<int>();
+            for (int i = 0; i < m_maxValues.Length; i++)
+            {
+                int temp = Random.Range(0, m_maxValues[i]);
+                value.Add(temp);
+            }
+            Stats stats = new Stats(value[0], value[1], value[2], value[3], value[4], m_maxValues);
+            SetStats(stats);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public void SetStats(Stats stats)
+    {
+        this.stats = stats;
+        stats.OnStatsChanged += Stats_OnStatsChanged;
+        UpdateStatsVisual();
+    }
+
+    private void Stats_OnStatsChanged(object sender, System.EventArgs e)
+    {
+        UpdateStatsVisual();
+    }
+
+    private void UpdateStatsVisual()
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[6];
+        Vector2[] uv = new Vector2[6];
+        int[] triangles = new int[3 * 5];
+
+        float angleIncrement = 360f / 5;
+
+        Vector3 attackVertex = Quaternion.Euler(0, 0, -angleIncrement * 0) * Vector3.up * radarChartSize * stats.GetStatAmountNormalized(Stats.Type.Attack, m_maxValues[0]);
+        int attackVertexIndex = 1;
+        Vector3 defenceVertex = Quaternion.Euler(0, 0, -angleIncrement * 1) * Vector3.up * radarChartSize * stats.GetStatAmountNormalized(Stats.Type.Defence, m_maxValues[1]);
+        int defenceVertexIndex = 2;
+        Vector3 speedVertex = Quaternion.Euler(0, 0, -angleIncrement * 2) * Vector3.up * radarChartSize * stats.GetStatAmountNormalized(Stats.Type.Speed, m_maxValues[2]);
+        int speedVertexIndex = 3;
+        Vector3 manaVertex = Quaternion.Euler(0, 0, -angleIncrement * 3) * Vector3.up * radarChartSize * stats.GetStatAmountNormalized(Stats.Type.Mana, m_maxValues[3]);
+        int manaVertexIndex = 4;
+        Vector3 healthVertex = Quaternion.Euler(0, 0, -angleIncrement * 4) * Vector3.up * radarChartSize * stats.GetStatAmountNormalized(Stats.Type.Health, m_maxValues[4]);
+        int healthVertexIndex = 5;
+
+        vertices[0] = Vector3.zero;
+        vertices[attackVertexIndex] = attackVertex;
+        vertices[defenceVertexIndex] = defenceVertex;
+        vertices[speedVertexIndex] = speedVertex;
+        vertices[manaVertexIndex] = manaVertex;
+        vertices[healthVertexIndex] = healthVertex;
+
+        uv[0] = Vector2.zero;
+        uv[attackVertexIndex] = Vector2.one;
+        uv[defenceVertexIndex] = Vector2.one;
+        uv[speedVertexIndex] = Vector2.one;
+        uv[manaVertexIndex] = Vector2.one;
+        uv[healthVertexIndex] = Vector2.one;
+
+        triangles[0] = 0;
+        triangles[1] = attackVertexIndex;
+        triangles[2] = defenceVertexIndex;
+
+        triangles[3] = 0;
+        triangles[4] = defenceVertexIndex;
+        triangles[5] = speedVertexIndex;
+
+        triangles[6] = 0;
+        triangles[7] = speedVertexIndex;
+        triangles[8] = manaVertexIndex;
+
+        triangles[9] = 0;
+        triangles[10] = manaVertexIndex;
+        triangles[11] = healthVertexIndex;
+
+        triangles[12] = 0;
+        triangles[13] = healthVertexIndex;
+        triangles[14] = attackVertexIndex;
+
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+
+        radarMeshCanvasRenderer.SetMesh(mesh);
+        radarMeshCanvasRenderer.SetMaterial(radarMaterial, radarTexture2D);
+    }
+}
